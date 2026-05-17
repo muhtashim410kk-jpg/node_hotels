a4f0dfa95776ab4b3900f875d12fa80472f905ae
Muhtashim <muhtashim410kk@gmail.com>
2026-05-18 01:12:58 +0500

    Added complete jwt token and get only access to protected route with jwt

--- jwt.js
+++ jwt.js
@@ -1,10 +1,31 @@
 const jwt = require('jsonwebtoken')
 
 
+const jwtauthmilddleware = (req,res,next)=>{
+
+    const authorization = req.headers.authorization
+    if(!authorization) return res.status(401).json({"message":"token not found"})
+
+      const token = req.headers.authorization.split(' ')[1]
+      if(!token) return res.status(401).json({"messge":"invalid token"})
+
+
+        
+              
+            const decoded = jwt.verify(token,process.env.SECRET_KEY)
+            req.user = decoded
+            next();
+
+        
+
+
+
+}
+
 
 const generatetoken = (Userdata)=>{
     return jwt.sign(Userdata,process.env.SECRET_KEY)
 
 }
 
-module.exports =generatetoken;
+module.exports = {jwtauthmilddleware,generatetoken}
--- routes/personroutes.js
+++ routes/personroutes.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const Person = require('./../models/person')
-const generatetoken =require('./../jwt')
+const {jwtauthmilddleware,generatetoken} =require('./../jwt')
 
 
-router.get('/',async(req,res)=>{
+router.get('/',jwtauthmilddleware,async(req,res)=>{
      
     try{
         
@@ -21,18 +21,75 @@ router.get('/',async(req,res)=>{
 
 router.post('/signup',async(req,res)=>{
     
-      
-         
+      try{
+        
         const Persondata = req.body;
         const newPersondata = new Person(Persondata);
 
         const response = await newPersondata.save();
-        res.status(200).json(response)
-        const token = generatetoken(response.username)
+          
+        const payload ={
+          id:response.id,
+          username: response.username
+        }
+
+
+        const token = generatetoken(payload)
         console.log("token is",token);
+        res.status(200).json({response:response,token:token})
+      }catch(error){
+        res.status(500).json({"message":"internal server error"})
+      }
+         
         
 }
 
 )
+
+router.post('/login',async(req,res)=>{
+
+     try{
+             
+      //extract username and password from req.body and then match it from database
+        const {username,password} = req.body;
+
+        const user = await Person.findOne({username:username})
+          
+        if(!user) return res.status(401).json({"message":"invalid username"})
+
+          if(user.password !== password){
+            res.status(401).json({"message":"incorrect password"})
+          }
+          
+           
+
+          const payload = {
+            id:user.id,
+            username:user.username
+          }
+
+          const token = generatetoken(payload)
+          console.log(token);
+          
+          if(!token)  return res.status(401).json({"message":"token not found"})
+
+            res.status(200).json({
+    message: "Login successful",
+    token: token
+});
+
+
+
+
+     }catch(error){
+              res.status(401).json({"message":"token not found"})
+
+     }
+
+
+
+})
+
+
 
 module.exports= router;
